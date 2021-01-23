e0cc7794665a4372f99168f589b5afb9bf0c06a5
umangsaluja <umangsaluja90@gmail.com>
2021-01-23 17:53:35 +0530

    Addd files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoList from "./Component-Folder/TodoList"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="todo-app">
+     <TodoList/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
